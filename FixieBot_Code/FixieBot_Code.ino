a46fb3d95bb416cba2d33a93c37bfa43eaf81ee2
Brian Davis <briandavism@gmail.com>
2020-02-02 00:12:14 +0100

    One slider is functioning live

--- FixieBot_Code/FixieBot_Code.ino
+++ FixieBot_Code/FixieBot_Code.ino
@@ -25,13 +25,21 @@ Adafruit_SSD1306 display(OLED_RESET);
 const int encoderTurnPin = 9;
 const int encoderPressPin = 8;
 
+int referenceVoltagePin = A0;
 int pot1Pin = A3;
 int pot2Pin = A2;
 int sliderPin = A1;
 
+int referenceVoltageValue = 0;
+int sliderValue = 0;
 int pot1Value = 0;
 int pot2Value = 0;
-int sliderValue = 0;
+
+
+int sliderTarget = 100;
+int pot1Target = 75;
+int pot2Target = 50;
+
 
 const int sound = 6;
 int encoderTurn = 2;
@@ -79,20 +87,74 @@ void loop() {
   //delay(1000);
   //display.display();
 
+
 //Display Loop
   display.clearDisplay();
+  
+//Bars mini game
+display.drawRect(0,0,128,64,WHITE); //Screen Frame
+
+//Get values from pots
+pot1Value = analogRead(pot1Pin);
+  delay(10);// this delay is to help the ADC differentiate the 3 signals
+  pot1Value = analogRead(pot1Pin);
+  
+  pot2Value = analogRead(pot2Pin);
+  delay(10);
+  pot2Value = analogRead(pot2Pin);
+  
+  sliderValue = analogRead(sliderPin);
+  delay(10);
+  sliderValue = analogRead(sliderPin);
+
+  referenceVoltageValue = analogRead(referenceVoltagePin);
+  delay(10);
+  referenceVoltageValue = analogRead(referenceVoltagePin);
+
+//the balance lines
+display.drawLine(4,20,123,20,WHITE); 
+display.drawLine(4,37,123,37,WHITE);
+display.drawLine(4,54,123,54,WHITE);
+
+//the target cursors
+display.drawRect((121-2)/100*sliderTarget+2,15,5,11,WHITE);
+display.drawRect((121-2)/100*pot1Target+2,32,5,11,WHITE);
+display.drawRect((121-2)/100*pot2Target+2,49,5,11,WHITE);
+
+int sliderPixel = (123-4)*(sliderValue/(float)referenceVoltageValue);
+//int pot1Pixel = 
+//int pot2Pixel = 
+
+
+//Current Values
+display.drawLine(sliderPixel+4,17,sliderPixel+4,23,WHITE);
+display.drawLine(11,34,11,40,WHITE);
+display.drawLine((123-4)/100*(pot2Value/referenceVoltageValue),51,(123-4)/100*(pot2Value/referenceVoltageValue),57,WHITE);
 
 //Power Bar
-  display.drawRect(1,44,126,20,WHITE);
-  display.fillRect(3,46,122*power/100,16,WHITE);
+  //display.drawRect(1,44,126,20,WHITE);
+  //display.fillRect(3,46,122*power/100,16,WHITE);
 
 //text example
   display.setTextSize(1);      // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.setCursor(0, 0);     // Start at top-left corner
+  display.setCursor(4, 4);     // Start at top-left corner
   display.cp437(true);         // Use full 256 char 'Code Page 437' font
-  pot1Value = analogRead(pot1Pin);
+  
+  
+
+/*
+  Print current values
+  display.print(F("Balance the scales"));
   display.print(pot1Value);
+  display.print(",");
+  display.print(pot2Value);
+  display.print(",");
+  display.print(sliderValue);
+  display.print(",");
+  display.print(referenceVoltageValue);
+*/
+
 
   // Show the display buffer on the screen. You MUST call display() after
   // drawing commands to make them visible on screen!
